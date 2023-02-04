00e8fa4199ad61b308b603979af040f18074361a
Aravindsuramalla <aravindsuramalla99@gmail.com>
2023-02-04 12:45:24 +0530

    index

---
+++ dom1.js
@@ -0,0 +1,30 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.url);
+//console.log(document.title);
+//document.title="Hi DOM";
+//console.log(document.type);
+//console.log(document.head);
+//console.log(document.all);
+//console.log(document.all[2]);
+//console.log(document.forms);
+
+//GEtELEMENTBYID
+
+//let headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent="Hello";
+//headerTitle.innerText="GOOGBYE";
+
+//headerTitle.innerHTML='<h3>hello</h3';
+//headerTitle.style.borderBottom='solid 3px #000';
+
+//QUERYSELECTOR
+
+//let addItem=document.querySelector('.title');
+//addItem.style.fontWeight='bold';
+//addItem.style.color="green";
+
+
+
+
