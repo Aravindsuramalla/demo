3f808ec74ff663fe4e8f484e51c966a0d42334ea
Aravindsuramalla <aravindsuramalla99@gmail.com>
2023-02-04 17:29:18 +0530

    fifth

--- dom1.js
+++ dom1.js
@@ -53,12 +53,69 @@
 //let li=document.querySelectorAll('li');
 //li[1].style.color='green';
 
-let odds=document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odds.length;i++)
-{
-    odds[i].style.backgroundColor='green';
-}
+//let odds=document.querySelectorAll('li:nth-child(odd)');
+//for(let i=0;i<odds.length;i++)
+//{
+   // odds[i].style.backgroundColor='green';
+//}
 
 
+let itemList=document.querySelector('#items');
+
+//console.log(itemList);
+//console.log(itemList.parentNode);
+//console.log(itemList.parentNode.parentNode);
+//itemList.parentNode.style.backgroundColor='grey';
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor='yellow';
+
+//console.log(itemList.childNodes);
+//console.log(itemList.children);
+//itemList.children[1].style.color='red';
+
+//console.log(itemList.firstChild);
+//console.log(itemList.firstElementChild);
+//console.log(itemList.lastChild);
+//console.log(itemList.firstElementChild);
+
+//console.log(itemList.nextSibling);
+//console.log(itemList.nextElementSibling);
+
+//console.log(itemList.previousSibling);
+//console.log(itemList.previousElementSibling);
+
+//create Element
+    //create Div
+
+let newDiv=document.createElement('div');
+newDiv.className='Hello';
+newDiv.id='world';
+
+//Add Attribute
+
+newDiv.setAttribute('title',"Hello Div");
+let newDivText=document.createTextNode('Hello!This is Aravind');
+//Add text to div
+newDiv.appendChild(newDivText);
+
+//Add newDiv to html
+
+let container=document.querySelector('header .container');
+let h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+console.log(newDiv);
 
 
+let newDiv2=document.createElement('div');
+newDiv2.className='hi';
+newDiv2.id='div2';
+
+newDiv2.setAttribute('title','Hello div2');
+let newDivText2=document.createTextNode('Hello World');
+
+newDiv2.appendChild(newDivText2);
+
+let listGroup=document.querySelector('.list-group');
+let li=document.querySelector('.list-group-item');
+listGroup.insertBefore(newDivText2,li);
+console.log(newDiv2);
